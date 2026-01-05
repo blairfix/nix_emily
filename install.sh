39a5115bd10555526d179a8de080701b6dfb8eda
blairfix <blairfix@fastmail.com>
2026-01-04 19:34:29 -0700

    install

--- install.sh
+++ install.sh
@@ -2,8 +2,7 @@
 # install configs
 sudo cp *.nix /etc/nixos/
 
-
 # install nix_build
-mkdir /home/petra/bin
-cp yay /home/petra/bin
-cp nix_cleanup /home/petra/bin
+mkdir /home/emily/bin
+cp yay /home/emily/bin
+cp nix_cleanup /home/emily/bin
