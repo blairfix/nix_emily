39f3367dfde046ec78a11c5a3a0bfed1de6cadba
blairfix <blairfix@fastmail.com>
2026-01-04 18:07:36 -0700

    first commit

---
+++ borg_petra.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, ... }:
+{
+
+    # borg petra
+    #----------------------------------------
+
+    systemd.timers."borg_petra" = {
+	wantedBy = [ "timers.target" ];
+	timerConfig = {
+	    OnCalendar = "*-*-*  *:19:00";
+	    Persistent = "true";
+	    Unit = "borg_petra.service";
+	};
+    };
+
+    systemd.services."borg_petra" = {
+	serviceConfig = {
+	    Type = "simple";
+	    User = "petra";
+	    WorkingDirectory = "/home/petra/bin";
+	};
+	path = with pkgs; [ 
+	    bash
+	    borgbackup
+	];
+	script = ''
+	    bash /home/petra/bin/backup.sh
+	    '';
+    };
+
+}
---
+++ install.sh
@@ -0,0 +1,9 @@
+
+# install configs
+sudo cp *.nix /etc/nixos/
+
+
+# install nix_build
+mkdir /home/petra/bin
+cp yay /home/petra/bin
+cp nix_cleanup /home/petra/bin
