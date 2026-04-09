14a4b32d633395cc1ead863443c731ad2731c3c7
blairfix <blairfix@fastmail.com>
2026-04-08 19:04:50 -0600

    borg timer

--- borg_emily.nix
+++ borg_emily.nix
@@ -7,7 +7,7 @@
     systemd.timers."borg_emily" = {
 	wantedBy = [ "timers.target" ];
 	timerConfig = {
-	    OnCalendar = "*-*-*  *:19:00";
+	    OnCalendar= "*-*-* 03:00:00";
 	    Persistent = "true";
 	    Unit = "borg_emily.service";
 	};
